1a0540eae412fa47f2f64d192bb3993d318889d0
Soeren Vullriede <soeren.vu@gmail.com>
2018-04-27 21:22:05 +0200

    remove js files from src directory

--- src/UUIDGenerator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Created by Soeren on 02.07.2017.
- */
-exports.UUIDGenerator = {
-    uuid: () => { return ("" + 1e7 + -1e3 + -4e3 + -8e3 + -1e11).replace(/1|0/g, function () { return (0 | Math.random() * 16).toString(16); }); }
-};
