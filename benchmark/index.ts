ad3693ba07d0d3764490ec689a3d92453b2d0292
soeren <soeren.vu@gmail.com>
2022-01-28 18:49:06 +0100

    first look at assemblyscript

--- benchmark/index.ts
+++
@@ -1,5 +0,0 @@
-// import './array_iterate.bench';
-// import './map_array_set.bench';
-import './yagl.bench';
-import './example.bench';
-// import './entity.bench';
--- benchmark/web.ts
+++
@@ -1,8 +0,0 @@
-import { ECS, System } from '@curbl/ecs';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-window.ECS = new ECS();
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-window.System = System;
